d952e665d40b39ddc9c556cd6ab26e8bd25d282c
Hellowalls1 <thesingingbutcher@gmail.com>
2020-10-01 14:49:11 -0500

    Team Model

--- TaskoProject/Models/Team.cs
+++ TaskoProject/Models/Team.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,23 @@ namespace TaskoProject.Models
 {
     public class Team
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+       
+        [StringLength(255)]
+        public string Photo { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+
     }
 }

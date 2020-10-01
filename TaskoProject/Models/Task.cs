9de5f13e2e34b5abdfa886e1a5e19f663a50f50a
Hellowalls1 <thesingingbutcher@gmail.com>
2020-10-01 16:04:20 -0500

    added task class

--- TaskoProject/Models/Task.cs
+++ TaskoProject/Models/Task.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,38 @@ namespace TaskoProject.Models
 {
     public class Task
     {
+        public int Id { get; set; }
+
+        [Required]
+        public int CreatorId { get; set; }
+        
+        //need to have the primary key for CreatorId and 
+        public UserProfile UserProfile { get; set; }
+
+
+        [Required]
+       
+        public int ListId { get; set; }
+
+        //primary key for list id
+        public List List { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(2000)]
+        public string Description { get; set; }
+
+
+        [Required]
+       
+        public DateTime DateCreated { get; set; }
+
+
+        [Required]
+        
+        public DateTime DueDate { get; set; }
     }
 }

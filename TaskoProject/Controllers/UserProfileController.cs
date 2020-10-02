e93acd0ccf79010a48c245939bc3229135f5892c
Hellowalls1 <thesingingbutcher@gmail.com>
2020-10-02 16:33:09 -0500

    recreated userprofilecontroller

--- TaskoProject/Controllers/UserProfileController.cs
+++ TaskoProject/Controllers/UserProfileController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TaskoProject.Data;
+using TaskoProject.Models;
+using TaskoProject.Repositories;
 
 namespace TaskoProject.Controllers
 {
@@ -11,5 +14,30 @@ namespace TaskoProject.Controllers
     [ApiController]
     public class UserProfileController : ControllerBase
     {
+        private readonly UserProfileRepository _userProfileRepository;
+        public UserProfileController(ApplicationDbContext context)
+        {
+            _userProfileRepository = new UserProfileRepository(context);
+        }
+
+        [HttpGet("{firebaseUserId}")]
+        public IActionResult GetByFirebaseUserId(string firebaseUserId)
+        {
+            var userProfile = _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+            return Ok(userProfile);
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserProfile userProfile)
+        {
+           
+            _userProfileRepository.Add(userProfile);
+            return CreatedAtAction(
+                nameof(GetByFirebaseUserId), new { firebaseUserId = userProfile.FirebaseUserId }, userProfile);
+        }
     }
 }

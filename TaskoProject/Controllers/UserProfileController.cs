43589113724c42160d98b44079ceeca34a598106
Hellowalls1 <thesingingbutcher@gmail.com>
2020-10-02 14:44:44 -0500

    continuation

---
+++ TaskoProject/Controllers/UserProfileController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskoProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+    }
+}

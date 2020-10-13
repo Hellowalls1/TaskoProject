01bc65092d8447503474bbee50313d7f00f20256
Hellowalls1 <thesingingbutcher@gmail.com>
2020-10-12 21:19:28 -0500

    fixed sql script and fixed null issue in project class

---
+++ TaskoProject/Controllers/ProjectController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskoProject.Data;
+using TaskoProject.Models;
+using TaskoProject.Repositories;
+
+namespace TaskoProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly TeamRepository _teamRepository;
+        private readonly UserProfileRepository _userProfileRepository;
+        private readonly ProjectRepository _projectRepository;
+
+        public ProjectController(ApplicationDbContext context)
+        {
+            _teamRepository = new TeamRepository(context);
+            _userProfileRepository = new UserProfileRepository(context);
+            _projectRepository = new ProjectRepository(context);
+        }
+
+        //getting the authorized user
+        private UserProfile GetCurrentUserProfile()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+        }
+
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            return Ok(_projectRepository.GetAll());
+        }
+
+        [HttpGet("id/{id}")]
+        public IActionResult GetPojectById(int id)
+        {
+            return Ok(_projectRepository.GetProjectById(id));
+        }
+
+        [HttpPost]
+        public IActionResult Post(Project project)
+        {
+            _projectRepository.Add(project);
+            return CreatedAtAction("Get", new { id = project.Id }, project);
+        }
+
+        //updates the team
+
+        [HttpPut("{id}")]
+
+        public IActionResult Put(int id, Project project)
+        {
+            if (id != project.Id)
+            {
+                return BadRequest();
+            }
+
+            _projectRepository.Update(project);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Delete(int id)
+        {
+            _projectRepository.Delete(id);
+            return NoContent();
+        }
+
+    }
+}
+
---
+++ TaskoSeedData.sql
@@ -0,0 +1,38 @@
+﻿
+set identity_insert [Team] on
+
+insert into [Team] ([Id], [Name], [Photo], [Description], [DateCreated]) values (1, 'Team 1', NULL, 'The Best Team', '2020-09-30');
+insert into [Team] ([Id], [Name], [Photo], [Description], [DateCreated]) values (2, 'Team 2', NULL, 'The Worst Team', '2020-09-30');
+
+set identity_insert [Team] off
+
+set identity_insert [UserProfile] on
+
+insert into [UserProfile] (Id, FirebaseUserId, FirstName, LastName, Email, JobTitle, AboutMe, TeamId, Photo) values (1, '2YTEuTVQ6ZfS5DQe37OwVCYspi73', 'Eva', 'Sanford', 'eva@eva.com', 'Social Worker', 'I like to better society', 1, null);
+insert into [UserProfile] (Id, FirebaseUserId, FirstName, LastName, Email, JobTitle, AboutMe, TeamId, Photo) values (2, 'xLnsaNrKhyRIEiNbCFX5I2CfZMI2', 'Tim', 'Jones', 'tim@tim.com', 'Software Developer', 'I do so many things.', 1, null);
+insert into [UserProfile] (Id, FirebaseUserId, FirstName, LastName, Email, JobTitle, AboutMe, TeamId, Photo) values (3, 'mvCoM3hKeOQ0mgWFbRblnYojfiH2', 'Sam', 'Smith', 'sam@sam.com', 'Junior Developer', 'I dont really do much of anything.', 2, null);
+
+set identity_insert [UserProfile] off
+
+set identity_insert [Project] on
+insert into [Project] ([Id], [UserProfileId], [TeamId], [Name], [Description], [DateCreated], [DueDate]) values (1, 1, 1, 'Preparing For New Management', 'We have a number of things to take care of before new corporate management takes over.', '2020-10-01', NULL);
+insert into [Project] ([Id], [UserProfileId], [TeamId], [Name], [Description], [DateCreated], [DueDate]) values (2, 2, 2, 'List Application', 'We need to get the list working that shows all the tasks.', '2020-10-01', NULL);
+
+set identity_insert [Project] off
+
+set identity_insert [List] on
+insert into [List] ([Id], [UserProfileId], [ProjectId], [Name], [Description], [DateCreated], [DueDate]) values (1, 1, 1, 'Clean the Office', 'We need to clean the office in order to get things ready for the new employee.', '2020-10-01', NULL);
+insert into [List] ([Id], [UserProfileId], [ProjectId], [Name], [Description], [DateCreated], [DueDate]) values (2, 2, 2, 'Get the list working', 'We need to get the list working that shows all the tasks.', '2020-10-01', NULL);
+
+set identity_insert [List] off
+
+set identity_insert [Task] on
+
+insert into [Task] ([Id], [UserProfileId], [ListId], [Name], [Description], [DateCreated], [DueDate]) values (1, 1, 1, 'Step 1', 'Do the stuff.', '2020-10-01', NULL);
+insert into [Task] ([Id], [UserProfileId], [ListId], [Name], [Description], [DateCreated], [DueDate]) values (2, 2, 1, 'Step 2', 'Do the stuff.', '2020-10-01', NULL);
+insert into [Task] ([Id], [UserProfileId], [ListId], [Name], [Description], [DateCreated], [DueDate]) values (3, 2, 2, 'Step 1', 'Do the stuff.', '2020-10-01', NULL); 
+insert into [Task] ([Id], [UserProfileId], [ListId], [Name], [Description], [DateCreated], [DueDate]) values (4, 3, 1, 'Step 3', 'Do the stuff.', '2020-10-01', NULL);
+
+set identity_insert [Task] off
+
+

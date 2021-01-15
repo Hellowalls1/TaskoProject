d7c216d340b21042f8c98e8ce00838c9783a85aa
Hellowalls1 <thesingingbutcher@gmail.com>
2021-01-15 15:01:41 -0600

    task list

--- TaskoProject/client/src/components/Task/Task.js
+++ TaskoProject/client/src/components/Task/Task.js
@@ -3,15 +3,21 @@ import { Link } from "react-router-dom";
 import { Card, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 
 const Task = ({ task }) => {
-  <div className="task-container">
-    <Card className="task-card">
-      <CardTitle className="task-title">{task.name}</CardTitle>
-      <CardBody className="task-body">
-        <p className="task-person">Assigned: {task.userProfile.name}</p>
-        <p className="task-description">{task.description}</p>
-        <p className="task-dueDate">{task.dueDate}</p>
-        <p className="task-dateCreated">{task.dateCreated}</p>
-      </CardBody>
-    </Card>
-  </div>;
+  return (
+    <>
+      <div className="task-container">
+        <Card className="task-card">
+          <CardTitle className="task-title">{task.name}</CardTitle>
+          <CardBody className="task-body">
+            <p className="task-person">Assigned: {task.userProfile.name}</p>
+            <p className="task-description">{task.description}</p>
+            <p className="task-dueDate">{task.dueDate}</p>
+            <p className="task-dateCreated">{task.dateCreated}</p>
+          </CardBody>
+        </Card>
+      </div>
+    </>
+  );
 };
+
+export default Task;
